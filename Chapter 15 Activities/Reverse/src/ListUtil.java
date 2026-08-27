cb6598c4a83b609dc3c99fd8aa357b52c5e698ba
SKDondapati <skdondapati@stu.naperville203.org>
2026-08-27 15:10:36 -0500

    Did not complete Reverse

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 /**
  * This class supplies a utility method to reverse the entries in a linked list.
@@ -12,6 +13,26 @@ public class ListUtil
     */
     public static void reverse(LinkedList<String> strings)
     {
-        ...
+        ListIterator<String> iterator = strings.listIterator();
+        int count = 0;
+        String first = "";
+        while (iterator.hasNext()) {
+            if (count == 0) {
+                first = iterator.next();
+            }
+            count++;
+        }
+        iterator = strings.listIterator();
+        iterator.next();
+        iterator.remove();
+        strings.addLast(first);
+        String currValue = "";
+        for (int i = 1; i < count; i++) {
+            iterator = strings.listIterator();
+            currValue = iterator.next();
+            iterator.remove();
+            strings.addLast(currValue);
+        }
+        System.out.println(strings);
     }
 }
